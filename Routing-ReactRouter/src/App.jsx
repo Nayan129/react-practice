ca056a8bd182224b940be40017772b5387bd5a66
Nayan <nayanbhusari02@gmail.com>
2026-05-19 08:39:05 +0530

    Add initial setup for React Router with Vite and Tailwind CSS

---
+++ Routing-ReactRouter/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-neutral-700 h-screen text-white font-bold text-xl p-4">
+      App
+    </div>
+  );
+};
+
+export default App;
