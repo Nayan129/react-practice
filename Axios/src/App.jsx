d2ef63bff3c37d1aa229bbe9f103cee5c4d96b64
Nayan <nayanbhusari02@gmail.com>
2026-05-24 13:19:04 +0530

    axios working done

--- Axios/src/App.jsx
+++ Axios/src/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import axios from "axios";
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [data, setData] = useState([]);
 
-export default App
+  async function fetchData() {
+    const response = await axios.get("https://fakestoreapi.com/products");
+    console.log(response.data);
+    setData(response.data);
+  }
+
+  return (
+    <div className="flex items-start justify-center pt-6 bg-neutral-600 h-screen flex-wrap gap-5">
+      <button
+        onClick={() => fetchData()}
+        className="bg-blue-600 p-2 rounded-lg cursor-pointer active:scale-95 hover:bg-blue-500 text-white font-semibold"
+      >
+        FetchData
+      </button>
+
+      {data.map((item, index) => {
+        console.log(item.image);
+
+        return (
+          <div key={item.id} className="border-2 p-2 bg-white rounded w-60">
+            <h1>{item.title}</h1>
+
+            <img
+              src={item.image}
+              alt="image"
+              className="w-full h-40 object-cover"
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default App;
